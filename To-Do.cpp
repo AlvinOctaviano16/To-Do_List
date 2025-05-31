b796e8034dbd2aea425b9e2750ff446a939152d3
AlvinOctaviano16 <octaviano1604@gmail.com>
2025-06-01 01:20:45 +0800

    Testing 2

--- To-Do.cpp
+++ To-Do.cpp
@@ -1,5 +1,16 @@
 #include<bits/stdc++.h>
+#define ll long long
+#define nl '\n'
+#define pb push_back
+#define ppb pop_back
+#define all(x) x.begin(), x.end()
+#define All(x) x.rbegin(), x.rend()
+#define fi first
+#define se second
+const int MAX=1e6;
+using namespace std;
 
 int main(){
+    cout<<"Hello"<<nl;
     return 0;
 }
