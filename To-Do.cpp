e6f9a42fdcac2147274ba693accab5b8e9fd3d58
AlvinOctaviano16 <octaviano1604@gmail.com>
2025-06-01 18:16:19 +0800

    Membuat fungsi untuk Create File/ Load File untuk file List

--- To-Do.cpp
+++ To-Do.cpp
@@ -10,7 +10,29 @@
 const int MAX=1e6;
 using namespace std;
 
+void showOpt(){
+    cout<<"========= Octaviano-Do List ========="<<nl<<"1. Tampilkan semua list"<<nl<<"2. Tambah Tugas Baru"<<nl<<"3. Hapus Tugas"<<nl<<"4. Exit"<<nl;
+}
+
+void load_file(vector<string>&task,const string &file){
+    ifstream ifs(file);
+    if(!ifs.is_open()){
+        ofstream ofs(file);
+        return;
+    }
+
+    string read;
+    while(getline(ifs,read)){
+        if(!read.empty())task.pb(read);
+    }
+    ifs.close();
+}
+
 int main(){
-    cout<<"Hello"<<nl;
+    vector<string>list;
+    const string file_list="todo.txt";
+    load_file(list,file_list);
+    for(auto it:list)cout<<it<<nl;
+    
     return 0;
 }
